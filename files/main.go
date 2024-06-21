6c15a6370a016023cdba63cdb7e55016364933c5
SYsZSY <SYsZSY@cyber-dojo.org>
2024-06-21 07:38:05 +0000

    5 ran tests, predicted red, got red

--- files/main.go
+++ files/main.go
@@ -8,7 +8,19 @@ func Wrap(text string, width int) (result string) {
     for _, p := range strings.Split(text, "\n") {
         if len(p) <= width {
             result += p + "\n"
-        }
+        } else {
+            ls := 0
+            for _, w := range strings.Split(p, " ") {
+                ws := len(w) + 1
+                if ls + ws <= width {
+                    result += w
+                    ls += ws
+                } else {
+                    result += "\n" + w
+                    ls = 0
+                }
+            }    
+        }    
     }    
     
     return
