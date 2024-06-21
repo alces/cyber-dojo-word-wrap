5b38c35e299edce9bd18967c5fe631b0f0b13080
SYsZSY <SYsZSY@cyber-dojo.org>
2024-06-21 07:50:54 +0000

    15 ran tests, predicted green, got green

--- files/main.go
+++ files/main.go
@@ -6,7 +6,7 @@ import (
 
 func Wrap(text string, width int) (result string) {
     for _, paragraph := range strings.Split(text, "\n") {
-        if len(p) <= width {
+        if len(paragraph) <= width {
             result += paragraph + "\n"
         } else {
             line := ""
