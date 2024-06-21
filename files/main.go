a7aa8f59a6222ae8bdad659e42997be7c4dfcdeb
SYsZSY <SYsZSY@cyber-dojo.org>
2024-06-21 07:50:35 +0000

    14 ran tests, predicted green, got amber

--- files/main.go
+++ files/main.go
@@ -5,12 +5,12 @@ import (
 )
 
 func Wrap(text string, width int) (result string) {
-    for _, p := range strings.Split(text, "\n") {
+    for _, paragraph := range strings.Split(text, "\n") {
         if len(p) <= width {
-            result += p + "\n"
+            result += paragraph + "\n"
         } else {
             line := ""
-            for _, w := range strings.Split(p, " ") {                
+            for _, w := range strings.Split(paragraph, " ") {                
                 if len(line) + len(w) + 1 < width {
                     if line != "" {
                         line += " "
