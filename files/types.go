987b13d058ad6343fc17bae75934fd87152136a6
SYsZSY <SYsZSY@cyber-dojo.org>
2024-07-03 08:18:40 +0000

    45 ran tests, predicted red, got red

--- files/types.go
+++ files/types.go
@@ -4,6 +4,15 @@ type wrapper struct {
     width int
 }
 
+type paragraph struct {
+    wrapper
+    text string
+}
+
+func (w *wrapper) newParagraph(text string) *paragraph {
+    return nil
+}
+
 func newWrapper(width int) *wrapper {
     return &wrapper{
         width: width,
--- files/types_test.go
+++ files/types_test.go
@@ -5,6 +5,10 @@ import (
     "github.com/stretchr/testify/assert"
 )
 
+func TestNewParagraph(t *testing.T) {
+    assert.NotNil(t, newWrapper(10).newParagraph("test"))
+}
+
 func TestNewWrapper(t *testing.T) {
     w := newWrapper(5)
     
